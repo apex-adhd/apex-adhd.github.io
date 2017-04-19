15eb11ae46fd6ff087f920f36e327bba21e7f83b
ekottkamp <betsy.kottkamp@gmail.com>
2017-04-18 22:01:21 -0700

    j

--- contact_me.php
+++ contact_me.php
@@ -1,7 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
+    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
