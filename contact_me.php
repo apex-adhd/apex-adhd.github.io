49b374853825dd07ca23d547468e6ba199ce2e85
ekottkamp <betsy.kottkamp@gmail.com>
2017-04-24 21:48:59 -0700

    updates!
    updates!

--- contact_me.php
+++ contact_me.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
-    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
